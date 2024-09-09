70571f5759a16ee4c6bef4176bc104104c562071
Jannassl <Jan.nassling@metropolia.fi>
2024-09-09 15:15:43 +0300

    j

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
-class TemperatureConverterTest {
-  
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TemperatureConverterTest {
+
+    TemperatureConverter converter = new TemperatureConverter();
+
+
+    @org.junit.jupiter.api.Test
+    public void testFahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32), 0.01);
+        assertEquals(100, converter.fahrenheitToCelsius(212), 0.01);
+        assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.01);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(32, converter.celsiusToFahrenheit(0), 0.01);
+        assertEquals(212, converter.celsiusToFahrenheit(100), 0.01);
+        assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.01);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testIsExtremeTemperature() {
+        assertTrue(converter.isExtremeTemperature(-50));
+        assertFalse(converter.isExtremeTemperature(20));
+        assertTrue(converter.isExtremeTemperature(60));
+    }
 }
