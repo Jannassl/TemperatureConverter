9cdf365ce6e60d1d4afb67ff5daefe7d80a423cd
Jannassl <Jan.nassling@metropolia.fi>
2024-09-22 20:29:01 +0300

    Week5_branch tehty

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -7,24 +7,29 @@ public class TemperatureConverterTest {
     TemperatureConverter converter = new TemperatureConverter();
 
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void testFahrenheitToCelsius() {
         assertEquals(0, converter.fahrenheitToCelsius(32), 0.01);
         assertEquals(100, converter.fahrenheitToCelsius(212), 0.01);
         assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.01);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void testCelsiusToFahrenheit() {
         assertEquals(32, converter.celsiusToFahrenheit(0), 0.01);
         assertEquals(212, converter.celsiusToFahrenheit(100), 0.01);
         assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.01);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void testIsExtremeTemperature() {
         assertTrue(converter.isExtremeTemperature(-50));
         assertFalse(converter.isExtremeTemperature(20));
         assertTrue(converter.isExtremeTemperature(60));
     }
+    @Test
+    public void testKelvinToCel() {
+        assertEquals(0.0, TemperatureConverter.kelvinToCel(273.15), 0.001);
+    }
+
 }
